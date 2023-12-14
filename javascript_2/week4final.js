6526dedaf9f2655f10b2aa33b43a311b625646f7
habibaadam <habiepalmer@gmail.com>
2023-12-14 14:08:28 +0000

    updating javascript

--- javascript_2/week4final.js
+++ javascript_2/week4final.js
@@ -1,30 +1,35 @@
-let Form1 = document.querySelector("#search-check");
-let apiKey = "80c24a3665db24tbfafc1c0035ca3o90";
-
 //Function for manipulating response from API
 function getResponse(response) {
   // Selecting HTML and changing value to actual real temperature
+  let cityNow = document.querySelector("#city-now");
+  cityNow.innerHTML = response.data.city;
   let weatherShowing = document.querySelector("#currentW");
   if (response.data && response.data.temperature && response.data.temperature.current !== undefined) {
     weatherShowing.innerHTML = Math.round(response.data.temperature.current);
   } else {
     //If the city is invalid or does not exist
     alert("Please enter a valid city name")
-    weatherShowing.innerHTML = "NaN";
+    weatherShowing.innerHTML = "✘";
 
   }
 }
 
-Form1.addEventListener("submit", function switchUp(event) {
+function citySearch(city) {
+  let apiKey = "80c24a3665db24tbfafc1c0035ca3o90";
+  let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(url).then(getResponse);
+}
+
+function switchUp(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-me");
-  let cityNow = document.querySelector("#city-now");
-  //Changing H1 according to search input
-  cityNow.innerHTML = searchInput.value;
-  if (searchInput.value) {
-    let url = `https://api.shecodes.io/weather/v1/current?query=${searchInput.value}&key=${apiKey}`;
-    axios.get(url).then(getResponse);
-  }});
+  citySearch(searchInput.value);
+}
+let Form1 = document.querySelector("#search-check");
+Form1.addEventListener("submit", switchUp);
+
+// Default city shown to user
+citySearch("Accra");
 
 // Changing dates to real time dates
 let rightNow = new Date();
